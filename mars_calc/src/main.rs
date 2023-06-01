5cc5c03a831a80a895603189cdd46c914f9cc45e
thehue <gouk32@naver.com>
2023-06-02 00:10:27 +0900

    lldb에서 소유권과 차용 탐구

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -4,11 +4,21 @@ fn main() {
     let mut input = String::new();
 
     io::stdin().read_line(&mut input);
-    println!("Input: {}", input);
 
-    let mut mars_weight = caculate_weight_on_mars(100.0);
-    mars_weight *= 1000.0;
-    println!("Weight on Mars: {}g", mars_weight)
+    borrow_string(&input);
+    own_string(input);
+
+    // println!("Input: {}", input);
+    // let mut mars_weight = caculate_weight_on_mars(100.0);
+    // println!("Weight on Mars: {}g", mars_weight)
+}
+
+fn borrow_string(s: &String) {
+    println!("{}", s);
+}
+
+fn own_string(s: String) {
+    println!("{}", s);
 }
 
 fn caculate_weight_on_mars(weight: f32) -> f32 {
