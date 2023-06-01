43e02264a76745209b05aa6fdc58aa5cba48b5d7
thehue <gouk32@naver.com>
2023-06-02 00:24:11 +0900

    화성에서 무게 코드 완료

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,24 +1,14 @@
 use std::io;
 
 fn main() {
+    println!("Enter Your weight (kg)");
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).unwrap();
 
-    borrow_string(&input);
-    own_string(input);
-
-    // println!("Input: {}", input);
-    // let mut mars_weight = caculate_weight_on_mars(100.0);
-    // println!("Weight on Mars: {}g", mars_weight)
-}
-
-fn borrow_string(s: &String) {
-    println!("{}", s);
-}
-
-fn own_string(s: String) {
-    println!("{}", s);
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = caculate_weight_on_mars(weight);
+    println!("Weight on Mars: {}g", mars_weight)
 }
 
 fn caculate_weight_on_mars(weight: f32) -> f32 {
