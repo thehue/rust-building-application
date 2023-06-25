1d457871fef1f94d1212c1d54d87ff16e5093c20
thehue <gouk32@naver.com>
2023-06-25 23:17:08 +0900

    27. 문자열 강의

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
-    server.run() // 리턴하지 않고 TCP 소켓에 생긴 새로운 연결을 기다리며 영원히 반복
+    let addr = "127.0.0.1:8080".to_string();
+    // or let addr = String::from("127.0.0.1:8080");
+    let server = Server::new(addr);
+    server.run()
 }
 
 struct Server {
@@ -12,8 +14,7 @@ impl Server {
         Self { addr }
     }
 
-    // self - 구조체의 인스턴스
-    // run() 함수를 나가게 되면 구조체는 실제로 할당이 해제됨
-    //  함수를 마칠 때 구조체를 할당 해제하고 싶지 않다면 &self로 지정 - 값 변경시에는 &mut self
-    fn run(self) {}
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
 }
