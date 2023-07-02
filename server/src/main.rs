cef90de1a61667eca10bb3f87b1c4e7116e96efe
thehue <gouk32@naver.com>
2023-07-02 16:41:43 +0900

    28. enums

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,5 @@
 fn main() {
     let addr = "127.0.0.1:8080".to_string();
-    // or let addr = String::from("127.0.0.1:8080");
     let server = Server::new(addr);
     server.run()
 }
@@ -17,4 +16,28 @@ impl Server {
     fn run(self) {
         println!("Listening on {}", self.addr);
     }
+}
+
+/*
+GET /user?id=10 HTTP/1.1
+HEADERS
+BODY
+ */
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    CONNECT,
+    HEAD,
+    OPTIONS,
+    TRACE,
 }
