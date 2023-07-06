df0492abe3f8e6249de06d409bc7bdf4dbee3731
thehue <gouk32@naver.com>
2023-07-06 23:31:14 +0900

    30. 모듈로 나누기

---
+++ server/src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    CONNECT,
+    HEAD,
+    OPTIONS,
+    TRACE,
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,43 +1,12 @@
+use http::Method;
+use http::Request;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let addr = "127.0.0.1:8080".to_string();
     let server = Server::new(addr);
     server.run()
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-/*
-GET /user?id=10 HTTP/1.1
-HEADERS
-BODY
- */
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    PATCH,
-    CONNECT,
-    HEAD,
-    OPTIONS,
-    TRACE,
 }
