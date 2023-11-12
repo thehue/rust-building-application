bd5c6c08adc846e1e9d77f16b05b00383445e3e2
thehue <gouk32@naver.com>
2023-11-12 22:21:15 +0900

    53. TCP Stream에 데이터 기록하기

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,7 +1,6 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::convert::TryInto;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -22,16 +21,25 @@ impl Server {
             match listener.accept() {
                 Ok((mut stream, _)) => {
                     let mut buffer = [0; 1024];
-                    let result = stream.read(&mut buffer);
-                    match result {
+                    match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {:?}", String::from_utf8_lossy(&buffer));
-                            let parsed_request_result = Request::try_from(&buffer[..]);
-                            match parsed_request_result {
+
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(
+                                        StatusCode::Ok,
+                                        Some("<h1>IT WORKS!!</h1>".to_string()),
+                                    )
                                 }
-                                Err(e) => println!("Failed to parse a request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse a request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                }
+                            };
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send respone: {}", e);
                             }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
