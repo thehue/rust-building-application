d30ec43f24369cbd18c3ead98c741af9ab941161
thehue <gouk32@naver.com>
2023-11-19 21:52:28 +0900

    55. 커스텀 트레이트

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,14 @@
 #![allow(dead_code)]
 
 use server::Server;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main() {
     let addr = "127.0.0.1:8080".to_string();
     let server = Server::new(addr);
-    server.run()
+    server.run(WebsiteHandler)
 }
