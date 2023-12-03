694424c70c2abfdadfb0c156733ef093b2e0fd8c
thehue <gouk32@naver.com>
2023-12-03 21:10:59 +0900

    60. 해킹을 막고 안전하게 파일을 제공하는 방법

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -12,7 +12,21 @@ impl WebsiteHandler {
     }
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(error) => {
+                println!("error:  {}", error);
+                None
+            }
+        }
     }
 }
 
@@ -22,7 +36,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::BadRequest, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
